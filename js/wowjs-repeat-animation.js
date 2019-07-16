3c1c3212f3f8254108b05714adbe7237c50ea52d
john alex <johnalexql@gmail.com>
2019-07-16 10:43:18 -0500

    advanced landing b

---
+++ js/wowjs-repeat-animation.js
@@ -0,0 +1,17 @@
+WOW.prototype.addBox = function(element) {
+    this.boxes.push(element);
+  };
+
+  // Init WOW.js and get instance
+  var wow = new WOW();
+  wow.init();
+
+  // Attach scrollSpy to .wow elements for detect view exit events,
+  // then reset elements and add again for animation
+  $('.wow').on('scrollSpy:exit', function() {
+    $(this).css({
+      'visibility': 'hidden',
+      'animation-name': 'none'
+    }).removeClass('animated');
+    wow.addBox(this);
+  }).scrollSpy();
